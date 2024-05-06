f0d69dfccd77fc61f6e3d8cee98742fa3a6e3b05
Luiz Guilherme <luizgpelegatti@gmail.com>
2024-05-06 18:19:57 -0300

    Calculadora release candidate

--- script.js
+++ script.js
@@ -16,12 +16,16 @@ function insert(input){
 function getValue(opr){
     oprValue = opr;
     stValue = display.value;
-    display.innerText = '';
-    display.value = '0';
+    display.innerText = '0';
+    display.value = 0;
 }
 
 function calculate(){
+    if(display.innerText === '0'){
+        ndValue = stValue;
+    } else {
     ndValue = display.value;
+    }
     switch (oprValue){
         case '+':
             rdValue = stValue + ndValue;
@@ -35,12 +39,23 @@ function calculate(){
         case '/':
             rdValue = stValue / ndValue;
             break
+        case '^':
+            rdValue = stValue ** ndValue;
+            break
     }
-    display.innerText = rdValue;
-    display.value = rdValue;
-    stValue = rdValue;
+    if(rdValue === 0){
+        rdValue = stValue;
+    } else {
+        display.innerText = rdValue;
+        display.value = rdValue;
+        stValue = rdValue;
+    }
+}
+
+function negative(){
+    display.value = -(display.value);
 }
 
 function clearDisplay(){
-    display.innerText = '0'; display.value = 0; 
+    display.innerText = '0'; display.value = 0; stValue = 0; ndValue = 0; rdValue = 0;
 }
